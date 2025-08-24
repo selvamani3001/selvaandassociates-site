7a52706c0fad7062a7775553991cd6f32147f479
selvamani <selvaandassociates3007@gmail.com>
2025-08-24 23:30:29 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,51 @@
+<!-- Structured Data (JSON-LD) -->
+<script type="application/ld+json">
+{
+  "@context": "https://schema.org",
+  "@type": "ProfessionalService",
+  "name": "Selva and Associates",
+  "image": "https://selvamani3007.github.io/selvaandassociates-site/images/selvamani.jpeg",
+  "@id": "https://selvamani3007.github.io/selvaandassociates-site/",
+  "url": "https://selvamani3007.github.io/selvaandassociates-site/",
+  "telephone": "+91-9500986143",
+  "priceRange": "₹₹",
+  "address": {
+    "@type": "PostalAddress",
+    "streetAddress": "No.4, Second Floor, Subbu Complex, Ram Nagar Main Road",
+    "addressLocality": "Tiruppur",
+    "addressRegion": "Tamil Nadu",
+    "postalCode": "641602",
+    "addressCountry": "IN"
+  },
+  "geo": {
+    "@type": "GeoCoordinates",
+    "latitude": 11.1081,
+    "longitude": 77.3411
+  },
+  "openingHoursSpecification": [
+    {
+      "@type": "OpeningHoursSpecification",
+      "dayOfWeek": [
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday"
+      ],
+      "opens": "09:00",
+      "closes": "19:00"
+    },
+    {
+      "@type": "OpeningHoursSpecification",
+      "dayOfWeek": "Sunday",
+      "opens": "ByAppointment",
+      "closes": "ByAppointment"
+    }
+  ],
+  "sameAs": [
+    "https://www.linkedin.com/in/selvamani3007",
+    "https://www.facebook.com/selvaandassociates"
+  ]
+}
+</script>
